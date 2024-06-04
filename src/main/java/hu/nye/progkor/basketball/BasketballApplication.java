566b7ecbcdfcfd92a5893e76a035b3f022b5adf8
unknown <gilanyiadrian@gmail.com>
2024-06-04 22:28:04 +0200

    First commit

---
+++ src/main/java/hu/nye/progkor/basketball/BasketballApplication.java
@@ -0,0 +1,15 @@
+package hu.nye.progkor.basketball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point of the application,
+ * Where the spring get initialized.
+ */
+@SpringBootApplication
+public class BasketballApplication {
+ 	public static void main(String[] args) {
+		SpringApplication.run(BasketballApplication.class, args);
+	}
+}
