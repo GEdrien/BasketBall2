91552296c5238da897d48c0e42a9515ecb5584d4
unknown <gilanyiadrian@gmail.com>
2024-06-05 00:19:38 +0200

    HH

--- src/main/java/hu/nye/progkor/basketball/BasketballApplication.java
+++ src/main/java/hu/nye/progkor/basketball/BasketballApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Entry point of the application,
  * Where the spring get initialized.
+ * Another Comment
  */
 @SpringBootApplication
 public class BasketballApplication {
